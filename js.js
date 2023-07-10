213e9de814d36d3701971dad6cd10fc7c4060ffe
Kai Love <ferretbeansqwq@gmail.com>
2023-07-11 04:15:06 +0800

    started about me (plus js); :3

--- js.js
+++ js.js
@@ -1,4 +1,15 @@
-//changes index.css to about.css
+//btn func
 function aboutme() {
-    
+    //changes title
+    document.title = "FerretBeans<3 - About";
+
+    //switches page
+    document.getElementsByTagName("link")[0].href = "about.css";
+    document.getElementsByTagName("body")[0].innerHTML = '';
+
+    //changes page text 
+    var heading = document.createElement("h1");
+    heading.textContent ="About Me";
+    heading.setAttribute("id", "test");
+    document.body.appendChild(heading);
 }
