fb156319f4311c19a48ef62e5b40d369fa889149
Alice <alice@ferretbeans.dev>
2024-12-27 14:08:22 +0800

    rg

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ function pfp() {
     fetch("https://discord.com/api/v10/users/938014927262711819", {
         method: "GET",
         mode: "cors",
-        credentials: "same-origin",
+        credentials: "include",
         headers: {
             "Authorization": "MTMyMjA3MDk5NTQ2NDQ5MTAxOA.GqDfF5.y6wWlgOeuYiL2q6Kg-A99b4k0Pe2yIUVHM3EIE",
             "Authorization": "Bot"
