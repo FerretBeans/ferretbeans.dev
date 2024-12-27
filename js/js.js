ee654b3985894634557e67db05c3ddfbcdb8db44
Alice <alice@ferretbeans.dev>
2024-12-27 13:54:22 +0800

    im dumb just kill me alr

--- js/js.js
+++ js/js.js
@@ -4,12 +4,12 @@
 function pfp() {
     fetch("https://discord.com/api/v10/users/938014927262711819", {
         method: "GET",
-        headers: [
-            "Authorization: Bot MTMyMjA3MDk5NTQ2NDQ5MTAxOA.GqDfF5.y6wWlgOeuYiL2q6Kg-A99b4k0Pe2yIUVHM3EIE"
-        ]
+        headers: {
+            "Authorization": "Bot MTMyMjA3MDk5NTQ2NDQ5MTAxOA.GqDfF5.y6wWlgOeuYiL2q6Kg-A99b4k0Pe2yIUVHM3EIE"
+        }
     })
     .then(res => res.json())
-    .then(res => console.log(res.json()))
+    .then(data => console.log(data))
 }
 
 pfp();
