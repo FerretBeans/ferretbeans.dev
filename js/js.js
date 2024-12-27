72dd34e2070b912f1f2910fc2c143f336b7fbd6f
Alice <alice@ferretbeans.dev>
2024-12-27 14:03:02 +0800

    kms

--- js/js.js
+++ js/js.js
@@ -4,6 +4,8 @@
 function pfp() {
     fetch("https://discord.com/api/v10/users/938014927262711819", {
         method: "GET",
+        mode: "cors",
+        credentials: "same-origin",
         headers: {
             "Authorization": "Bot MTMyMjA3MDk5NTQ2NDQ5MTAxOA.GqDfF5.y6wWlgOeuYiL2q6Kg-A99b4k0Pe2yIUVHM3EIE"
         }
