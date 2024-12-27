901a46d4852b1ede173f60d268bed7cce721f5e1
Alice <alice@ferretbeans.dev>
2024-12-27 13:51:36 +0800

    am i stupid??

--- js/js.js
+++ js/js.js
@@ -8,6 +8,8 @@ function pfp() {
             "Authorization: Bot MTMyMjA3MDk5NTQ2NDQ5MTAxOA.GqDfF5.y6wWlgOeuYiL2q6Kg-A99b4k0Pe2yIUVHM3EIE"
         ]
     });
+    this(res => res.json)
+    this(res => console.log(res.json))
 }
 
 pfp();
