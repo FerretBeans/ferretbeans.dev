ed70973e63ce2beef620ac83628700f1ef720203
Alice <alice@ferretbeans.dev>
2025-03-07 22:17:08 +0800

    used wrong url xD

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ function pfp() {
     .then(response => response.json())
     .then(data => {
         var meow = document.getElementById("img");
-        meow.src = `https://discord.com/api/v10/users/870628551823609926/${data.avatar}.png`
+        meow.src = `https://cdn.discordapp.com/avatars/870628551823609926/${data.avatar}.png`
     });
 }
 
