3a87d9040eaee71dae6bd7b452d8592be51b86e0
Alice <alice@ferretbeans.dev>
2025-03-07 22:14:40 +0800

    i think it will all work now ;w;

--- js/js.js
+++ js/js.js
@@ -4,7 +4,8 @@ function pfp() {
     fetch("https://cors-dun.vercel.app/discord?userId=870628551823609926")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        var meow = document.getElementById("img");
+        meow.src = `https://discord.com/api/v10/users/870628551823609926/${data.avatar}.png`
     });
 }
 
