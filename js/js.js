e78df9877f6cda89466b9c79f15f8702914acaf7
Alice <alice@ferretbeans.dev>
2025-03-07 22:06:39 +0800

    forgot to remove comments for my adhd

--- js/js.js
+++ js/js.js
@@ -4,7 +4,7 @@ function pfp() {
     fetch("https://cors-dun.vercel.app/discord?userId=870628551823609926")
     .then(response => response.json())
     .then(data => {
-        console.log(data);  // The data returned from Discord API
+        console.log(data);
     });
 }
 
