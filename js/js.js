0f2b2524c48d9c379157f660cfc6d392668cafc2
Alice <alice@ferretbeans.dev>
2025-03-07 21:40:43 +0800

    i js made a vercel thingy

--- js/js.js
+++ js/js.js
@@ -1,19 +1,11 @@
 //literally just to get my discord pfp
 
 function pfp() {
-    const a = "MTE1MDA1NzcxMzY2MTkwNzAwNw.";
-    const b = "GzJAwx.";
-    const c = "9VwKOCtRhGcOiNOJ0o2_MXjeFmr_z3wGh4lLgw";
-
-    fetch("https://discord.com/api/v10/users/870628551823609926", {
-        method: "GET",
-        headers: {
-            "Authorization": `Bot ${a + b + c}`
-        }
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+    fetch("https://cors-one-alpha.vercel.app/discord?userId=870628551823609926")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);  // The data returned from Discord API
+    });
 }
 
 pfp();
