5c17392960673629909fe0354c31b9fb14ea5045
Alice <alice@ferretbeans.dev>
2025-03-07 22:06:22 +0800

    i forgot to updfate the url lmao

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 //literally just to get my discord pfp
 
 function pfp() {
-    fetch("https://cors-one-alpha.vercel.app/discord?userId=870628551823609926")
+    fetch("https://cors-dun.vercel.app/discord?userId=870628551823609926")
     .then(response => response.json())
     .then(data => {
         console.log(data);  // The data returned from Discord API
