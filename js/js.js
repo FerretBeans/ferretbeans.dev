5c11581a346c147434eeb379a2c297e3d5741bcd
Alice <alice@ferretbeans.dev>
2025-03-10 01:15:10 +0800

    got new rainbow text!

--- js/js.js
+++ js/js.js
@@ -1,3 +1,6 @@
+//VARIABLES!!!!!! >w<
+
+
 //literally just to get my discord pfp
 function pfp() {
     fetch("https://cors-dun.vercel.app/discord?userId=870628551823609926")
@@ -8,57 +11,32 @@ function pfp() {
     });
 }
 
-//im adding a way for my name to be rainbow :3
-    var speed=100;
-    var glow=0;
-    var raincol=new Array("#ff0000", "#ff5500", "#ffaa00", "#ffff00", "#aaff00", "#55ff00", "#00ff00", "#00ff55", "#00ffaa", "#00ffff", "#00aaff", "#0055ff", "#0000ff", "#5500ff", "#aa00ff", "#ff00ff", "#ff00aa", "#ff0055"); // change the colours if you want to
-    var alink="https://ferretbeans.dev";
-    
-    /****************************
-    *    Rainbow Text Effect    *
-    *(c)2003-13 mf2fm web-design*
-    *  http://www.mf2fm.com/rv  *
-    * DON'T EDIT BELOW THIS BOX *
-    ****************************/
-    var rainbow, raintxt, raincnt=0;
-    
-    function addLoadEvent(funky) {
-      var oldonload=window.onload;
-      if (typeof(oldonload)!='function') window.onload=funky;
-      else window.onload=function() {
-        if (oldonload) oldonload();
-        funky();
+/*
+im adding a way for my name to be rainbow :3
+TAKEN FROM https://codepen.io/rileyjshaw/pen/vExWpe !!!!!
+editted a little to make faster but still go show love <3
+*/ 
+(function () {
+    var angle = 0;
+    var p = document.getElementById('x3');
+    var text = p.textContent.split('');
+    var len = text.length;
+    var phaseJump = 180 / len;
+    var spans;
+  
+    p.innerHTML = text.map(function (char) {
+      return '<span>' + char + '</span>';
+    }).join('');
+  
+    spans = p.children;
+  
+    (function wheee () {
+      for (var i = 0; i < len; i++) {
+        spans[i].style.color = 'hsl(' + (angle + Math.floor(i * phaseJump)) + ', 55%, 70%)';
       }
-    }
-    
-    addLoadEvent(regenbogen);
-    
-    function regenbogen() { if (document.getElementById) {
-      var i, rainbeau;
-      rainbow=document.getElementById("rainbow");
-      raintxt=rainbow.firstChild.nodeValue;
-      while (rainbow.childNodes.length) rainbow.removeChild(rainbow.childNodes[0]);
-      for (i=0; i<raintxt.length; i++) {
-        rainbeau=document.createElement("span");
-        rainbeau.setAttribute("id", "rain"+i);
-        rainbeau.appendChild(document.createTextNode(raintxt.charAt(i)));
-        if (alink) {
-          rainbeau.style.cursor="pointer";
-          rainbeau.onclick=function() { top.location.href=alink; }
-        }
-        rainbow.appendChild(rainbeau);
-      }
-      rainbow=setInterval ("raining()", speed);
-    }}
-    
-    function raining() {
-      var i, c;
-      for (i=0; i<raintxt.length; i++) {
-        c=raincol[(i+raincnt)%raincol.length];
-        document.getElementById("rain"+i).style.color=c;
-        if (glow) document.getElementById("rain"+i).style.textShadow=c+" 0px 0px "+glow+"px";
-      }
-      raincnt++;
-    }
+      angle += 3;
+      requestAnimationFrame(wheee);
+    })();
+})();
 
 pfp();
