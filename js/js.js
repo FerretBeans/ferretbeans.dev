fbbe6c616108d6c1aa924031c69e04279fc827f5
Alice <alice@ferretbeans.dev>
2025-11-07 01:01:28 +0800

    changed it back bcs proxy didnt work

--- js/js.js
+++ js/js.js
@@ -4,7 +4,7 @@ function pfp() {
     .then(response => response.json())
     .then(data => {
         var meow = document.getElementById("img");
-        meow.src = `https://corsproxy.io/?url=https://cdn.discordapp.com/avatars/870628551823609926/${data.avatar}.png`
+        meow.src = `https://cdn.discordapp.com/avatars/870628551823609926/${data.avatar}.png`;
     });
 }
 
